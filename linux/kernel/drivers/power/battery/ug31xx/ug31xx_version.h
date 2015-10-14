a0e403eac74ec4ee2035440ea05b2b2e6687d5d7
Shaka Huang <shakalaca@gmail.com>
2015-10-14 14:33:09 +0800

    V12.16.4.2

---
+++ linux/kernel/drivers/power/battery/ug31xx/ug31xx_version.h
@@ -0,0 +1,9 @@
+#define UG31XX_DRIVER_VERSION       (110)
+#define UG31XX_DRIVER_VERSION_STR   ("110")
+#define UG31XX_DRIVER_RELEASE_DATE  ("20140710-070130")
+#define UG31XX_DRIVER_RELEASE_NOTE  ("645:5e")
+
+/*
+	Release Note
+	1. Update the ggb version after updating ggb file from daemon
+*/
